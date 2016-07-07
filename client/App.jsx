ae46a67d5ba56df4530a40cea30c399c8634aa65
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2016-07-07 23:11:51 +0300

    Added first draft of UI prototype

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
+import { Button, Form, Grid, Icon, Input } from 'stardust';
 
 export default class App extends Component {
+  renderNewForm() {
+    return (
+      <Form>
+        <Form.Field>
+          <Input className="action" placeholder="Paste your long URL here">
+            <Button type="submit" className="primary">
+              <Icon className="save" />
+              Shorten
+            </Button>
+          </Input>
+        </Form.Field>
+      </Form>
+    );
+  }
+
+  renderSavedForm() {
+    return (
+      <Form>
+        <Form.Field>
+          <Input className="action" placeholder="">
+            <Button type="submit" className="primary">
+              <Icon className="external" />
+              Redirect
+            </Button>
+          </Input>
+        </Form.Field>
+      </Form>
+    );
+  }
+
   render() {
     return (
-      <span>Render whole app here</span>
+      <Grid className="middle aligned center aligned">
+        <Grid.Column>
+          {this.renderSavedForm()}
+        </Grid.Column>
+      </Grid>
     );
   }
 }
