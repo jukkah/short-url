0463a4b6bfa74a51347f57291a642c7bafe48ec9
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2016-07-08 23:48:04 +0300

    Temporarily fixed double jQuery loading

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,9 @@ module.exports = {
     publicPath: '/build/',
     filename: 'bundle.js',
   },
+  externals: {
+    jquery: 'jQuery',
+  },
   devServer: {
     contentBase: './client',
     hot: true,
